7f8e3f00ebec689bb1b5192f8f01f1269eaf8a69
Muhammad Ammad <ammadmughal567@gmail.com>
2025-06-07 14:57:58 +0500

    Added projects

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -4,6 +4,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { ProjectCardSchema } from './Projects'
+import { Button } from './ui/button'
+import { GithubIcon, NetworkIcon } from 'lucide-react'
 // import ReactGA from 'react-ga4'
 
 const ProjectCard = ({ project, variants }: ProjectCardSchema) =>
@@ -41,8 +43,20 @@ const ProjectCard = ({ project, variants }: ProjectCardSchema) =>
             </div>
 
             <div className="flex flex-row items-center mt-2 mb-[-1] px-6 justify-between">
-                <h3 className="text-black dark:text-white text-base font-semibold">{project.name}</h3>
-                <h5 className="text-primary text-sm">{project.type}</h5>
+                <h2 className="text-black dark:text-white text-xl font-semibold w-[80%]">{project.name}</h2>
+
+                <span className='w-20% flex gap-2'>
+                    <Link href={project.githubLink} target='_blank'>
+                        <GithubIcon size={14} />
+                    </Link>
+                    <div className='border-l-2' />
+                    <Link href={project.url} target='_blank'>
+                        <NetworkIcon size={14} />
+                    </Link>
+                </span>
+            </div>
+            <div className="flex flex-row items-center mt-2 mb-[-1] px-6 justify-between">
+                <h5 className="text-primary text-md">{project.type}</h5>
             </div>
 
             <hr className="mx-6 my-2 border-gray-300 dark:border-gray-500" />
--- src/lib/features/projects/projectsSlice.ts
+++ src/lib/features/projects/projectsSlice.ts
@@ -1,14 +1,110 @@
 import { createSlice } from '@reduxjs/toolkit';
-import eOps from '../../../assets/images/eOps.png';
-import MecpoSmart from '../../../assets/images/MEPCO Smart.jpeg';
-import Codiniti from '../../../assets/images/Codiniti.png';
-import CitizenMonitor from '../../../assets/images/Citizen Monitor.png';
-import OptForSmartMeters from '../../../assets/images/OptForSmartMeter.png'
-import ExamReady from '../../../assets/images/ExamReady.png'
-import AntiTheft from '../../../assets/images/AnitTheft.png'
+import eOps from '@/assets/images/eOps.png';
+import MecpoSmart from '@/assets/images/MEPCO Smart.jpeg';
+import Codiniti from '@/assets/images/Codiniti.png';
+import CitizenMonitor from '@/assets/images/Citizen Monitor.png';
+import OptForSmartMeters from '@/assets/images/OptForSmartMeter.png'
+import ExamReady from '@/assets/images/ExamReady.png'
+import AntiTheft from '@/assets/images/AnitTheft.png'
+import Kippax from '@/assets/images/kippax.png'
+import Crappo from '@/assets/images/crappo.png'
+import Spm from '@/assets/images/spm.png'
+import Sis from '@/assets/images/sis.png'
+import Chatiniti from '@/assets/images/chatiniti.png'
 
 const initialState = {
     projectsData: [
+        {
+            name: 'Kippax',
+            type: 'Single Page App',
+            urlTitle: 'kippax',
+            githubLink: 'https://github.com/AmmadMughal20/kippax-creative-canvas',
+            technologies: ['React', 'Tailwind CSS', 'Shadcn', 'Typescript'],
+            url: 'https://kippax-creative-canvas.vercel.app/',
+            image: Kippax,
+            video: {
+                src: './',
+                type: '',
+                controls: true,
+            },
+            description: `Developed a sleek, modern, and responsive website for Kippax, a dynamic marketing agency. Built using React, Tailwind CSS and Loveable AI, the site reflects the agency's creative brand with a bold, clean design and smooth user experience. I implemented a custom color scheme, optimized performance, and ensured mobile-first responsiveness. The project showcases my skills in React, Tailwind CSS, Lovable AI and responsive UI/UX, and building brand-aligned digital experiences for creative industries.`
+        },
+        {
+            name: 'Codiniti',
+            type: 'Single Page App',
+            urlTitle: 'Codiniti',
+            githubLink: 'https://github.com/AmmadMughal20/Codiniti',
+            technologies: ['React', 'Tailwind CSS', '', 'Javascript', 'Framer Motion'],
+            url: 'http://www.codiniti.com',
+            image: Codiniti,
+            video: {
+                src: '',
+                type: '',
+                controls: true,
+            },
+            description: `I developed an interactive website for a tech startup, showcasing the company's profile in a visually appealing manner. Utilizing Lattefiles for animations and Framer Motion for smooth transitions, the website offered an engaging user experience. With captivating motion effects and seamless section navigation, the website effectively communicated the startup's offerings, achievements, team, and vision.`
+        },
+        {
+            name: 'Crappo',
+            type: 'Responsive Web App',
+            urlTitle: 'crappo',
+            githubLink: 'https://github.com/AmmadMughal20/crappo',
+            technologies: ['Next.js', 'Tailwind CSS', 'Mongo Db', 'Typescript', 'Shadcn', 'Zod'],
+            url: 'https://crappo-nine.vercel.app/',
+            image: Crappo,
+            video: {
+                src: '',
+                type: '',
+                controls: true,
+            },
+            description: `CRAPPO is a responsive and visually engaging cryptocurrency website built with Next.js and Tailwind CSS. It features a modern landing page highlighting a blockchain project, with user registration/login, newsletter subscription utilizing MongoDB, and smooth animations using Framer Motion. The site is fully responsive and optimized for performance. My work helped the company establish a professional online presence, attract new users, and grow its subscriber base.`
+        },
+        {
+            name: 'SPM',
+            type: 'SAAS Web Application',
+            urlTitle: 'spm',
+            githubLink: 'https://github.com/AmmadMughal20/SPM',
+            technologies: ['React', 'Flask', 'Tailwind', 'Typescript', 'Python'],
+            url: '',
+            image: Spm,
+            video: {
+                src: '',
+                type: '',
+                controls: true,
+            },
+            description: `The Sabri Project Management (SPM) System is a responsive SaaS platform tailored for the flex signage and print media industry, enabling end-to-end management of advertising projects for clients like PEPSI, Coke, and telecom firms. Built using React, Flask (Python), SQLAlchemy, and REST APIs, it supports client, contact, and vendor management, detailed deliverable workflows (survey to execution), and multi-role access. The system ensures seamless project tracking, real-time updates, and efficient collaboration across teams in a scalable and secure environment.`
+        },
+        {
+            name: 'Inventory Management',
+            type: 'SAAS Web Application',
+            urlTitle: 'sis',
+            githubLink: 'https://github.com/AmmadMughal20/energy-flow-inventory-hub',
+            technologies: ['Flask', 'Jinja2', 'PostgreSql', 'SQLAlchemy', 'Javascript'],
+            url: '',
+            image: Sis,
+            video: {
+                src: '',
+                type: '',
+                controls: true,
+            },
+            description: `Store Inventory System: full-stack web app built with Flask, Python, JavaScript and Jinja2, supporting full process of a standard inventory management business flow along with CRUD for items, categories, and suppliers, hierarchical stores. I developed it with role-based access, user authentication, real-time stock updates, low-stock alerts, and data export (Excel/PDF). The responsive dashboard includes advanced search, filtering, date-wise stock tracking, and inventory movement logs. It ensures multi-store inventory management, centralized control, and improved operational efficiency.`
+        },
+        {
+            name: 'Chatiniti',
+            type: 'SAAS Web Application',
+            urlTitle: 'chatiniti',
+            githubLink: 'https://github.com/AmmadMughal20/Chatiniti',
+            technologies: ['Flask', 'Jinja2', 'SocketIO', 'WebRTC', 'Javascript'],
+            url: '',
+            image: Chatiniti,
+            video: {
+                src: '',
+                type: '',
+                controls: true,
+            },
+            description: `Developed a full-featured real-time chat application using Flask, Flask-SocketIO, and PostgreSQL. Chatiniti supports one-to-one and group chat, along with audio/video calls using WebRTC. Designed REST and WebSocket endpoints with JWT-based auth. Built responsive UI with Jinja, HTML/CSS and Tailwind CSS. Features include live messaging, typing indicators, and media calls. Deployed using Docker with clean and scalable backend architecture.`
+        },
+
         {
             name: 'e-Operations',
             type: 'SAAS Web Application',
@@ -39,21 +135,7 @@ const initialState = {
             },
             description: 'The Mepco Smart is a cross platform mobile as well as web interface developed as part of the Power Sector Improvement Activities (PSIA). It provides power consumers with smart monitoring features such as load profile, power outage information, consumption trends, and payment trends. The portal utilizes Highcharts.js to display interactive graphs and incorporates Formik and Yup for requesting various power services like new connections, attribute changes, meter replacements, due date extensions, and more.'
         },
-        {
-            name: 'Codiniti',
-            type: 'Single Page App',
-            urlTitle: 'Codiniti',
-            githubLink: 'https://www.codiniti.com',
-            technologies: ['React.js', 'Material UI', 'SCSS', 'Javascript', 'Framer Motion'],
-            url: 'http://www.codiniti.com',
-            image: Codiniti,
-            video: {
-                src: './CFC.mp4',
-                type: 'video/mp4',
-                controls: true,
-            },
-            description: `I developed an interactive website for a tech startup, showcasing the company's profile in a visually appealing manner. Utilizing Lattefiles for animations and Framer Motion for smooth transitions, the website offered an engaging user experience. With captivating motion effects and seamless section navigation, the website effectively communicated the startup's offerings, achievements, team, and vision.`
-        },
+
         {
             name: 'Opt for Smart Meter',
             type: 'Single Page Application',
